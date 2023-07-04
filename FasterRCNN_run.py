278fbbf9ecda26be99586181db2558b135233bec
kashish.kumar@exxonmobil.com <kashish.kumar@exxonmobil.com>
2023-07-04 06:59:27 +0000

    --

--- FasterRCNN_run.py
+++ FasterRCNN_run.py
@@ -1,6 +1,18 @@
 # Databricks notebook source
+!pwd
+
+# COMMAND ----------
+
 # MAGIC %sh
 # MAGIC pip install -r requirements.txt
+
+# COMMAND ----------
+
+!pip uninstall torch==1.12.1+cu113
+
+# COMMAND ----------
+
+!pip install albumentations
 
 # COMMAND ----------
 
@@ -14,6 +26,10 @@ torch.__version__
 
 # COMMAND ----------
 
+!pip list
+
+# COMMAND ----------
+
 if torch.cuda.is_available():
     print("CUDA is available!")
 else:
@@ -23,6 +39,33 @@ else:
 
 # MAGIC %sh
 # MAGIC python train.py --data data_configs/autoddc.yaml --epochs 100 --model fasterrcnn_resnet50_fpn --name autoddc --batch 16 
+
+# COMMAND ----------
+
+import matplotlib.pyplot as plt
+import glob as glob
+
+# COMMAND ----------
+
+results_dir_path = '/content/fastercnn-pytorch-training-pipeline/outputs/training/custom_training'
+valid_images = glob.glob(f"{results_dir_path}/*.jpg")
+
+for i in range(2):
+    plt.figure(figsize=(10, 7))
+    image = plt.imread(valid_images[i])
+    plt.imshow(image)
+    plt.axis('off')
+    plt.show()
+
+# COMMAND ----------
+
+# No verbose mAP.
+!python eval.py --weights outputs/training/autoddc/best_model.pth --config data_configs/autoddc.yaml --model fasterrcnn_resnet50_fpn_v2
+
+# COMMAND ----------
+
+# Verbose mAP.
+!python eval.py --weights outputs/training/autoddc/best_model.pth --config data_configs/autoddc.yaml --model fasterrcnn_resnet50_fpn_v2 --verbose
 
 # COMMAND ----------
 
